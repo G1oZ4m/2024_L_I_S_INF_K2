78a2dd77400657a93e60b710dbc6a68cb8371bb3
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-04-17 11:08:45 +0200

    akjsdg

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Oleksandr", "msg":Hello Worlddd!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Oleksandr", "msg":"Hello Worlddd!"}', rv.data)
